57bedaf3ab1f581a0dcc25d15ab3bc171bc655f4
Sotiris Sotiriou <sotiriou.sot@yahoo.com>
2022-09-08 02:40:15 +0300

    use jquery

--- calculator/script.js
+++ calculator/script.js
@@ -1,15 +1,19 @@
 function display(val){
-    document.getElementById('result').value += val;
+    // document.getElementById('result').value += val;
+    const prev_val = $("#result").val();
+    $("#result").val(prev_val+val);
     return val;
 }
 
 function clearResult(){
-    document.getElementById('result').value = "";
+    // document.getElementById('result').value = "";
+    $("#result").val("");
     return;
 }
 
 function solve(){
-    let x = document.getElementById('result').value;
+    // let x = document.getElementById('result').value;
+    let x = $("#result").val();
     let y = eval(x);
     document.getElementById('result').value = y;
     return y;
